995dde84de7aa591fd6cde4afa16a3a9ae616e39
DouShaoxun <dousx_cn@163.com>
2022-06-30 21:26:47 +0800

    RibbonClient

--- consumer/src/main/java/cn/cruder/dousx/or/consumer/config/FeignConfig.java
+++ consumer/src/main/java/cn/cruder/dousx/or/consumer/config/FeignConfig.java
@@ -2,11 +2,18 @@ package cn.cruder.dousx.or.consumer.config;
 
 import cn.cruder.dousx.or.api.feign.ProducerFeignApi;
 import cn.cruder.dousx.or.consumer.properties.ServicesUrlProperties;
+import com.netflix.client.ClientFactory;
+import com.netflix.client.config.ClientConfigFactory;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.BaseLoadBalancer;
+import com.netflix.loadbalancer.Server;
 import feign.Feign;
 import feign.Logger;
 import feign.Target;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
+import feign.ribbon.LBClient;
+import feign.ribbon.LBClientFactory;
 import feign.ribbon.RibbonClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
@@ -27,11 +34,26 @@ public class FeignConfig {
 
     @Bean
     public Feign feign() {
+        RibbonClient ribbonClient = RibbonClient.builder().lbClientFactory(new LBClientFactory() {
+            @Override
+            public LBClient create(String clientName) {
+                BaseLoadBalancer loadBalancer = new BaseLoadBalancer();
+                // TODO: 2022.06.30 根据 clientName从配置中读取
+                loadBalancer.addServer(new Server("127.0.0.1", 7070));
+                loadBalancer.addServer(new Server("127.0.0.1", 7071));
+                IClientConfig producer = ClientFactory.getNamedConfig("producer", ClientConfigFactory.DEFAULT::newConfig);
+                LBClient lbClient = LBClient.create(loadBalancer, producer);
+                return lbClient;
+            }
+        }).build();
         return Feign.builder()
                 .logger(new Logger.JavaLogger(this.getClass().getName()))
+                .client(ribbonClient)
                 .logLevel(Logger.Level.FULL)
                 .encoder(new JacksonEncoder())
                 .decoder(new JacksonDecoder())
                 .build();
     }
+
+
 }
--- consumer/src/test/java/cn/cruder/dousx/or/consumer/OpTests.java
+++ consumer/src/test/java/cn/cruder/dousx/or/consumer/OpTests.java
@@ -1,9 +1,9 @@
 package cn.cruder.dousx.or.consumer;
 
-import cn.cruder.dousx.or.api.feign.ProducerFeignApi;
+import cn.cruder.dousx.or.api.dto.GetResult;
 import cn.cruder.dousx.or.api.dto.PostParam;
 import cn.cruder.dousx.or.api.dto.PostResult;
-import cn.cruder.dousx.or.api.dto.GetResult;
+import cn.cruder.dousx.or.api.feign.ProducerFeignApi;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -32,10 +32,12 @@ public class OpTests {
 
     @Test
     void testPost() {
-        String token = "bran - 123";
-        PostParam param = new PostParam(UUID.randomUUID().toString(), "tom", 19);
-        PostResult postResult = producerFeignApi.postTest(token, param);
-        log.info(JSON.toJSONString(postResult));
+        for (int i = 0; i < 10; i++) {
+            String token = "bran - 123";
+            PostParam param = new PostParam(UUID.randomUUID().toString(), "tom", 19);
+            PostResult postResult = producerFeignApi.postTest(token, param);
+            log.info(JSON.toJSONString(postResult));
+        }
 
     }
 }
